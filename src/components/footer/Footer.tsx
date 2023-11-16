fa8dec56c61955dd5e418c5b364cd98a5c590d0e
Josianjidile <josianjidile@gmail.com>
2023-11-16 09:31:20 +0300

    Initial commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>jceeadmin</span>
+      <span>© jcee Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
